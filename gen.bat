fc3ad7ffec6449bc06494587b345c9ba7f332a32
Sava <50882455+sava41@users.noreply.github.com>
2023-12-05 16:32:02 -0500

    renamed level exporter script

--- gen.bat
+++ gen.bat
@@ -2,4 +2,4 @@ set RESOLUTION=100
 set SAMPLINGS=1024
 
 # Run the scripts
-python ./tools/process_level.py %1 ./output/ %RESOLUTION% %SAMPLINGS%
+python ./tools/export_level.py %1 ./output/ %RESOLUTION% %SAMPLINGS%
