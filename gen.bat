489ecd4ff2192b24075b10f7d475bdca404c5394
Sava <sava41@gmail.com>
2025-03-15 21:31:05 -0400

    updated project structure

--- gen.bat
+++
@@ -1,5 +0,0 @@
-set RESOLUTION=100
-set SAMPLINGS=1024
-
-# Run the scripts
-python ./tools/export_level.py %1 ./output/ %RESOLUTION% %SAMPLINGS%
---
+++ source/extblender/__init__.py
@@ -0,0 +1,52 @@
+bl_info = {
+    "name": "Add Cube Panel",
+    "author": "Your Name",
+    "version": (1, 0),
+    "blender": (3, 0, 0),
+    "location": "View3D > UI > Add Cube",
+    "description": "Adds a panel with a button to create a cube",
+    "category": "3D View",
+}
+
+import mftools
+import bpy
+from bpy.types import Panel, Operator
+
+class ADD_OT_cube(Operator):
+    """Add a cube to the scene"""
+    bl_idname = "object.add_cube_operator"
+    bl_label = "Add Cube"
+    
+    def execute(self, context):
+        # Add a cube at the 3D cursor location
+        bpy.ops.mesh.primitive_cube_add(location=bpy.context.scene.cursor.location)
+        return {'FINISHED'}
+
+class VIEW3D_PT_add_cube_panel(Panel):
+    """Creates a Panel in the 3D Viewport UI"""
+    bl_label = "Add Cube"
+    bl_idname = "VIEW3D_PT_add_cube_panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Add Cube'  # This will be the tab name
+    
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("object.add_cube_operator")
+
+# Registration
+classes = (
+    ADD_OT_cube,
+    VIEW3D_PT_add_cube_panel,
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+def unregister():
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+if __name__ == "__main__":
+    register()
