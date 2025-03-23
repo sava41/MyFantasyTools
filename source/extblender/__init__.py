72ed9bf89d74ec5fa63c56c65334a8e1dc16de9f
Sava <sava41@gmail.com>
2025-03-22 22:28:35 -0400

    added python package installation

--- source/extblender/__init__.py
+++ source/extblender/__init__.py
@@ -7,6 +7,11 @@ __addon_dir__ = os.path.dirname(os.path.abspath(__file__))
 if __addon_dir__ not in sys.path:
     sys.path.append(__addon_dir__)
 
+__external_path__ = os.path.join(os.path.dirname(__file__), "external")
+
+if __external_path__ not in sys.path:
+    sys.path.append(__external_path__)
+
 from . ui import ui_main
 
 bl_info = {
