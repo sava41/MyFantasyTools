9bb9d9f2c49b6b04e317515a4e5a3bc72677f35a
Sava <sava41@gmail.com>
2026-05-02 10:30:38 -0400

    Update import_plugin.gd

--- source/godot_plugin/resources/import_plugin.gd
+++ source/godot_plugin/resources/import_plugin.gd
@@ -33,39 +33,16 @@ func _get_import_options(path, preset_index):
 	return []
 
 func _import(source_file, save_path, options, r_platform_variants, r_gen_files):
-	var reader := ZIPReader.new()
-	var err := reader.open(source_file)
-	if err != OK:
-		return FileAccess.get_open_error()
-		
-	var level_file_path = ""
-	var level_name = "level"
+	DirAccess.copy_absolute(source_file, save_path + "." + _get_save_extension())
 
-	for file_path in reader.get_files():
-		if file_path.ends_with("/"):
-			if !DirAccess.dir_exists_absolute(file_path):
-				DirAccess.make_dir_recursive_absolute(file_path)
-		else:
-			var data = reader.read_file(file_path)
-			var file = FileAccess.open("res://" + file_path, FileAccess.WRITE)
-			file.store_buffer(data)
-			
-			if file_path.ends_with(".level"):
-				level_file_path = "res://" + file_path
-				level_name = file_path.get_basename()
-
-	reader.close()
-
-	DirAccess.copy_absolute(source_file,  save_path + "." + _get_save_extension())
-	
 	var current_scene_root = EditorInterface.get_edited_scene_root()
-	
+
 	if !current_scene_root:
 		return ERR_CANT_CREATE
-		
+
 	var level = MFLevel.new()
-	level.name = level_name
-	level.set_level_file_path(level_file_path)
+	level.name = source_file.get_file().get_basename()
+	level.set_level_file_path(source_file)
 	current_scene_root.add_child(level)
 	level.owner = current_scene_root
 
